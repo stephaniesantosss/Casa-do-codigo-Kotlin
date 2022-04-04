c2715c312b54c1ee97bda30cec465e1011164f81
stephaniesantosss <stephanieps2016@hotmail.com>
2022-04-04 14:10:43 -0300

    initial setup

---
+++ src/main/kotlin/com/codex/casadocodigo/CasaDoCodigoApplication.kt
@@ -0,0 +1,11 @@
+package com.codex.casadocodigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CasaDoCodigoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CasaDoCodigoApplication>(*args)
+}
---
+++ src/test/kotlin/com/codex/casadocodigo/CasaDoCodigoApplicationTests.kt
@@ -0,0 +1,8 @@
+package com.codex.casadocodigo
+
+class CasaDoCodigoApplicationTests {
+	
+	fun contextLoads() {
+	}
+
+}
