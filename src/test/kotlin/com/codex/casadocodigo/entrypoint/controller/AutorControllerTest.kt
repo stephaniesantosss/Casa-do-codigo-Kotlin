6422a78cd083ee634ddcc8ffd966b6e318f48ae0
stephaniesantosss <stephanieps2016@hotmail.com>
2022-04-27 11:52:59 -0300

    teste unitario de autorController implementado

---
+++ src/test/kotlin/com/codex/casadocodigo/entrypoint/controller/AutorControllerTest.kt
@@ -0,0 +1,44 @@
+package com.codex.casadocodigo.entrypoint.controller
+
+import com.codex.casadocodigo.core.model.dto.AutorIn
+import com.codex.casadocodigo.core.usecase.AutorUseCase
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import java.time.LocalDateTime
+
+@ExtendWith(MockitoExtension::class)
+internal class AutorControllerTest {
+
+    @InjectMocks
+    lateinit var autorController: AutorController
+
+    @Mock
+    val autorUseCase: AutorUseCase = Mockito.mock(AutorUseCase::class.java)
+
+    private lateinit var autorIn: AutorIn
+
+    @BeforeEach
+    fun setup() {
+        autorIn = autorIn()
+    }
+
+    @Test
+    fun deveSalvarAutorComSucesso() {
+        assertDoesNotThrow { autorController.salvaAutor(autorIn) }
+    }
+
+    fun autorIn(): AutorIn {
+        return AutorIn(
+                nome = "Stephanie",
+                email = "stephanie@gmail.com",
+                descricao = "aaaaaaa",
+                instanteRegistro = LocalDateTime.now()
+        )
+    }
+}
